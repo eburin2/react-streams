dff4d5b3e09481d83b6af324ee97e1f3b45aadd0
Elliot Burin <eburin2@gmail.com>
2019-10-04 12:23:10 -0700

    remove excess items from api server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import streams from '../apis/streams';
+import history from '../history';
+
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,8 +29,7 @@ export const createStream = formValues => async ( dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  // do some programmatic navigation to
-  // get the user back to the root route
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
